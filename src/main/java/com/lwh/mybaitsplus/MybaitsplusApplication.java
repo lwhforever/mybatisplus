4e110d1bcc90760f8f95676a66f6ac1ae842d273
lwhforever <1039001008@qq.com>
2022-07-14 00:07:09 +0800

    pull test again

--- src/main/java/com/lwh/mybaitsplus/MybaitsplusApplication.java
+++ src/main/java/com/lwh/mybaitsplus/MybaitsplusApplication.java
@@ -18,6 +18,8 @@ public class MybaitsplusApplication {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test again");
+        
     }
 
 }
